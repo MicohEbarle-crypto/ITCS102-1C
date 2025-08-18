63169fae5c05c6c248ef3114db9b9608e42ca965
MicohEbarle-crypto <micohebarle03@gmail.com>
2025-08-18 14:46:12 +0800

    Code_challenge2.py

---
+++ Code_challenge2.py
@@ -0,0 +1,20 @@
+peso = eval(input("Enter amount to deposited-"))
+print("1000: ", peso // 1000)
+peso %= 1000
+print("500: ", peso // 500) 
+peso %= 500
+print("200: ", peso // 200) 
+peso %= 200
+print("100: ", peso // 100) 
+peso %= 100
+print("50: ", peso // 50) 
+peso %= 50
+print("20: ", peso // 20) 
+peso %= 20
+print("10: ", peso // 10) 
+peso %= 10
+print("5: ", peso // 5) 
+peso %= 5
+print("1: ", peso // 1) 
+peso %= 1
+print("her is a breakdown of your deposit- ")
