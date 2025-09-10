4d795b2dd9099206b2c1d25bfdd2143eb0748876
MicohEbarle-crypto <micohebarle03@gmail.com>
2025-09-10 19:17:10 +0800

    Code_challeng4.py

--- Code_challeng4.py
+++ Code_challeng4.py
@@ -45,18 +45,18 @@ if a == 'mystery':
      if m == 'short': 
        i = input("how long decade? (19s,20s)")
        if i == '19s' :
-           print("I recomend is Rurouni Kenshin: Trust & Betrayal")
+           print("I recomend is Perfect Blue")
        if i == '20s' :
-           print("Akudama Drive")
+           print("I recommend is Death Note")
      if m == 'medium':
         i = input("how long decade? (19s,20s)")
         if i == '19s' :
-             print("I recomend is Trigun")
+             print("Monster")
         if i == '20s' :
-             print("I recomend isa My Hero Academia: Vigilantes ")
+             print("I recomend i 20th Century Boys")
      if m == 'large' :
         i = input("how long decade? (19s,20s)")
         if i == '19s' :
-            print("I recomend is Dragon Ball Z")
+            print("I recomend is Detective Conan")
         if i == '20s' :
-            print("I recomend isa Jujutsu Kaisen")
+            print("I recomend is Higurashi no Naku Koro ni franchise")
