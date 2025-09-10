f788d3c0655b6b9e27cb3bdc5265d672a4fac56e
MicohEbarle-crypto <micohebarle03@gmail.com>
2025-09-10 20:45:03 +0800

    Activity11.py

---
+++ Activity11.py
@@ -0,0 +1,13 @@
+a = eval(input("input your temperature?"))
+if a <= 20 :
+    print("your temperature is cool")
+elif a <= 30:
+        print("your temperature is cold")
+elif a <= 40:
+        print("your temperature is warm")
+elif a <= 50:
+        print("your temperature is hot")
+elif a <= 60:
+        print("your temperature is extremely heat")
+else :
+  print("incorrect your temperature ")
