21579dcbbe6f6792569cfb746282f8244eacefeb
MicohEbarle-crypto <micohebarle03@gmail.com>
2025-09-10 19:03:28 +0800

    Code_challeng4.py

---
+++ Code_challeng4.py
@@ -0,0 +1,62 @@
+print("Hi! welcome manga readers I recomender ") 
+a = input("what genre do you like? (action, fantasy, mystery)") 
+if a == 'action': 
+     m = input("how its long? (short, medium, large)") 
+     if m == 'short': 
+       i = input("how long decade? (19s,20s)")
+       if i == '19s' :
+           print("I recomend is Rurouni Kenshin: Trust & Betrayal")
+       if i == '20s' :
+           print("Akudama Drive")
+     if m == 'medium':
+        i = input("how long decade? (19s,20s)")
+        if i == '19s' :
+             print("I recomend is Trigun")
+        if i == '20s' :
+             print("I recomend isa My Hero Academia: Vigilantes ")
+     if m == 'large' :
+        i = input("how long decade? (19s,20s)")
+        if i == '19s' :
+            print("I recomend is Dragon Ball Z")
+        if i == '20s' :
+            print("I recomend isa Jujutsu Kaisen")
+if a == 'fantasy': 
+     m = input("how its long? (short, medium, large)") 
+     if m == 'short': 
+       i = input("how long decade? (19s,20s)")
+       if i == '19s' :
+           print("I recomend is Rurouni Kenshin: Trust & Betrayal")
+       if i == '20s' :
+           print("Akudama Drive")
+     if m == 'medium':
+        i = input("how long decade? (19s,20s)")
+        if i == '19s' :
+             print("I recomend is Trigun")
+        if i == '20s' :
+             print("I recomend isa My Hero Academia: Vigilantes ")
+     if m == 'large' :
+        i = input("how long decade? (19s,20s)")
+        if i == '19s' :
+            print("I recomend is Dragon Ball Z")
+        if i == '20s' :
+            print("I recomend isa Jujutsu Kaisen")
+if a == 'mystery': 
+     m = input("how its long? (short, medium, large)") 
+     if m == 'short': 
+       i = input("how long decade? (19s,20s)")
+       if i == '19s' :
+           print("I recomend is Rurouni Kenshin: Trust & Betrayal")
+       if i == '20s' :
+           print("Akudama Drive")
+     if m == 'medium':
+        i = input("how long decade? (19s,20s)")
+        if i == '19s' :
+             print("I recomend is Trigun")
+        if i == '20s' :
+             print("I recomend isa My Hero Academia: Vigilantes ")
+     if m == 'large' :
+        i = input("how long decade? (19s,20s)")
+        if i == '19s' :
+            print("I recomend is Dragon Ball Z")
+        if i == '20s' :
+            print("I recomend isa Jujutsu Kaisen")
