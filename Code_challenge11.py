3d7799698e084ea3a68febe826adab1ae0f70ccf
MicohEbarle-crypto <micohebarle03@gmail.com>
2025-10-02 19:27:18 +0800

    Code_challenge11.py

---
+++ Code_challenge11.py
@@ -0,0 +1,9 @@
+print("\t\t\t\t *", end=' ')
+for m in range(1,10,1):
+    for i in range(10,m,-1):
+        print(' ', end=' ')
+    for c in range(1,m,1):
+        print('*', end=' ')
+    for o in range(1,m,1):
+        print('*', end=' ')
+    print()
