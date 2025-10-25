cb577b7e559fdab3b44432f94cbacff678cfd380
MicohEbarle-crypto <micohebarle03@gmail.com>
2025-10-25 20:39:12 +0800

    Update Code_challenge14.py

--- Code_challenge14.py
+++ Code_challenge14.py
@@ -1,1 +1,15 @@
-gshhs
+anime_list = [ ]
+
+micoh= True
+while micoh == True:
+    anlist = input("Enter an anime (or type \"exit\" to stop): ")
+
+    if anlist == "Exit":
+        print("You are exited to the anime ----> ")
+        break
+    else:
+        animelist.append(anlist)
+        continue
+print("Here is the list of animes ---> ")
+for a in animelist:
+    print(f"-{a}")
