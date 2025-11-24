8b54c885ac4e851d76b63f2c52da6fbdd761a767
MicohEbarle-crypto <micohebarle03@gmail.com>
2025-11-24 21:27:17 +0800

    Activity25.py

---
+++ Activity25.py
@@ -0,0 +1,10 @@
+#LIST
+EBARLE_FAMILY = ['FATHER', 'MOTHER', 'BROTHER', 'YOUNGEST', 'SISTER', 'ME', 'YOUNGEST' ]
+print(EBARLE_FAMILY)
+
+#DICTIONARY
+EBARLE_FAM = {} #empty dictionary
+# { KEY  VALUE } : dictionary has no index, it uses 'KEYS'
+
+EBARLE_FAM = {'chris' : 'FATHER', 'marissa' : 'MOTHER', 'MARK' : 'BROTHER', 'maecy' : 'SISTRR', 'micoh' : 'ME', 'charlo' : 'youngest' }
+print(EBARLE_FAM['micoh'])
