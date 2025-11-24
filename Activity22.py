91d8cf552ebdf2a3db3eb00e9d976e1d98c313fd
MicohEbarle-crypto <micohebarle03@gmail.com>
2025-11-24 21:02:32 +0800

    Implement list operations on family and name

---
+++ Activity22.py
@@ -0,0 +1,24 @@
+#.revers() 
+#.append(,item) 
+#.insert(index, item) 
+#.remove(item) 
+#.pop(index) 
+# len(list) 
+#.sort 
+Familiy = ['papa', 'mama', 'kuya', 'ate', 'ako', 'bunso"] 
+Familiy.reverse() 
+Familiy.append('bunso') 
+Familiy.insert(0, 'mama')
+Familiy.remove('ako') 
+Familiy.pop(3) 
+print(Familiy) 
+
+
+fullname = 'Micoh Ebarle' 
+faname = list(fullname) 
+frame.reverse() 
+fname.append('z') 
+fname.insert(1, 'x') 
+fname.remove('c') 
+fname.pop(0) 
+print(fname)
